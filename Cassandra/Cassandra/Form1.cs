e0077d0dfbbe18564e0c36de2e0a05ac2e59746f
WeizKrus <andri.gunawan@live.com>
2016-07-14 09:48:55 -0700

    initial commit

---
+++ Cassandra/Cassandra/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cassandra;
+using Cassandra.Data;
+using System.Diagnostics;
+
+namespace CassandraProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public void Connect(String node)
+        //{
+        //    var Cluster = Cluster.Builder()
+        //             .AddContactPoint(node)
+        //             .Build();
+        //    Console.WriteLine("Connected to cluster: " + Cluster.Metadata.ClusterName.ToString());
+        //    foreach (var host in Cluster.Metadata.AllHosts())
+        //    {
+        //        Console.WriteLine("Data Center: " + host.Datacenter + ", " +
+        //            "Host: " + host.Address + ", " +
+        //            "Rack: " + host.Rack);
+        //    }
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cluster cluster = Cluster.Builder().WithPort(9042).AddContactPoint("192.168.25.131").Build();
+            Debug.WriteLine(cluster.Metadata.ClusterName);
+
+            try
+            {
+                ISession session = cluster.Connect();
+                //session.Execute("CREATE KEYSPACE simplex WITH replication " +
+                //    "= {'class':'SimpleStrategy', 'replication_factor':3};");
+                session.Execute("insert into users (lastname, age, city, email, firstname) values ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')");;
+
+                Debug.WriteLine("Connected to cluster: " + cluster.Metadata.ClusterName.ToString());
+                foreach (var host in cluster.Metadata.AllHosts())
+                {
+                    Debug.WriteLine(
+                        "Data Center: " + host.Datacenter + ", " + "Host: " + host.Address + ", " + "Rack: " + host.Rack);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cluster.Shutdown();
+            }
+        }
+    }
+}
---
+++ MongoDbTest/MongoDbTest/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Bson;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+namespace MongoDbTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MongoClient mongoClient = new MongoClient(@"mongodb://localhost:27017");
+            IMongoDatabase db = mongoClient.GetDatabase("test");
+            var collection = db.GetCollection<BsonDocument>("restaurants");
+
+            var filter = new BsonDocument();
+            var count = 0;
+
+            using (var cursor = collection.FindSync(filter))
+            {
+                while (cursor.MoveNext())
+                {
+                    var batch = cursor.Current;
+                    foreach (var document in batch)
+                    {
+                        string a = "";
+                        // process document
+                        count++;
+                    }
+                }
+            }
+
+            count.Should().Be(1);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public class address
+        {
+            public string street { get; set; }
+            public string zipcode { get; set; }
+            public string building { get; set; }
+            public int[] coord { get; set; }
+        }
+
+        public class grades
+        {
+            public DateTime date { get; set; }
+            public char grade { get; set; }
+            public byte score { get; set; }
+        }
+
+    }
+}
